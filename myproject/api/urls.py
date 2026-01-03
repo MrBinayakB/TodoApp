69f3532f921fe7a2d9c5974308111258a5b8b26a
MrBinayakB <b.binayak07@gmail.com>
2026-01-03 11:12:14 +0545

    Request method compiled into single function

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('getTask/',views.getTask),
-    path('addTask/', views.addTask),
+    path('tasks/',views.task_list),
+    path('tasks/<int:pk>',views.tasks_detail),
 
-    path('getUser/',views.getUser),
-    path('addUser/', views.addUser),
+    path('users/',views.user_list),
+    path('users/<int:pk>',views.users_detail),
 ]
--- myproject/api/views.py
+++ myproject/api/views.py
@@ -1,33 +1,82 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
 from mainapp.models import TaskModel
 from mainapp.models import UserModel
 from .serializers import TaskSerializer, UserSerializer
 
 #GET and POST method for Task
-@api_view(['GET'])
-def getTask(request):
-    tasks = TaskModel.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
-    return Response(serializer.data)
+@api_view(['GET','POST'])
+def task_list(request):
+    if request.method == 'GET':
+        tasks = TaskModel.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET','PUT','DELETE'])
+def tasks_detail(request, pk):
 
-@api_view(['POST'])
-def addTask(request):
-    serializer = TaskSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
+    try:
+        tasks = TaskModel.objects.get(pk=pk)
+    except TaskModel.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = TaskSerializer(tasks)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = TaskSerializer(tasks, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        tasks.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
 #Get and POST method for USer
-@api_view(['GET'])
-def getUser(request):
-    tasks = UserModel.objects.all()
-    serializer = UserSerializer(tasks, many=True)
-    return Response(serializer.data)
+@api_view(['GET','POST'])
+def user_list(request):
+    if request.method == 'GET':
+        user = UserModel.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
 
-@api_view(['POST'])
-def addUser(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def users_detail(request, pk):
+
+    try: 
+        user = UserModel.objects.get(pk=pk)
+    except UserModel.DoesNotExist:
+        return Response(status= status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer  = UserSerializer(user)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
